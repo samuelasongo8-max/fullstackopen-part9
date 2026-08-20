c8467e8b680ea91d41cd7b261ae49efaa7503d7c
Samuel Asongo <samuelasongo8@gmail.com>
2026-08-20 15:35:45 +0300

    Complete exercise 6 eslint

--- fullstackopen-part9/healthapp/eslint.config.mjs
+++ fullstackopen-part9/healthapp/eslint.config.mjs
@@ -25,15 +25,11 @@ export default tseslint.config({
     "@stylistic/semi": "error",
 
     "@typescript-eslint/no-unsafe-assignment": "error",
-
     "@typescript-eslint/no-explicit-any": "error",
 
     "@typescript-eslint/explicit-function-return-type": "off",
-
     "@typescript-eslint/explicit-module-boundary-types": "off",
-
     "@typescript-eslint/restrict-template-expressions": "off",
-
     "@typescript-eslint/restrict-plus-operands": "off",
 
     "@typescript-eslint/no-unused-vars": [

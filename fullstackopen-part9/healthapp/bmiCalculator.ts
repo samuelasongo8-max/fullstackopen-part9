2e5cc1928f1b6ad087f6c1e0efb6f19042422d57
Samuel Asongo <samuelasongo8@gmail.com>
2026-08-20 10:37:08 +0300

    Complete Exercise 1 Body mass index

---
+++ fullstackopen-part9/healthapp/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const heightInMeters = height / 100;
+  const bmi = weight / (heightInMeters * heightInMeters);
+
+  if (bmi < 18.5) {
+    return "Underweight range";
+  } else if (bmi < 25) {
+    return "Normal range";
+  } else if (bmi < 30) {
+    return "Overweight range";
+  } else {
+    return "Obese range";
+  }
+};
+
+console.log(calculateBmi(180, 74));

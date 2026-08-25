b8f938c3066c65c543cca7c9875f981966494476
Samuel Asongo <samuelasongo8@gmail.com>
2026-08-25 09:46:33 +0300

    Complete exercise 17 course step 1

---
+++ course/src/App.tsx
@@ -0,0 +1,67 @@
+type CoursePart = {
+  name: string;
+  exerciseCount: number;
+};
+
+type HeaderProps = {
+  name: string;
+};
+
+type ContentProps = {
+  parts: CoursePart[];
+};
+
+type TotalProps = {
+  parts: CoursePart[];
+};
+
+const Header = ({ name }: HeaderProps) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }: ContentProps) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }: TotalProps) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exerciseCount, 0);
+
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const courseName = 'Half Stack application development';
+
+  const courseParts: CoursePart[] = [
+    {
+      name: 'Fundamentals',
+      exerciseCount: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exerciseCount: 7,
+    },
+    {
+      name: 'Deeper type usage',
+      exerciseCount: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total parts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
